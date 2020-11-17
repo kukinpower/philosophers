3bf12b571d1d392f677a77fcb37dd355924fd3d1
Roman <thekukinpower@gmail.com>
2020-11-17 19:49:45 +0300

    add libft functions

--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -1,4 +1,15 @@
 #ifndef PHILOSOPHERS_PHILO_ONE_H
 #define PHILOSOPHERS_PHILO_ONE_H
 
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+size_t		ft_strlen(const char *s);
+void		ft_putstr_fd(char *s, int fd);
+char		*ft_itoa(int n);
+int			ft_atoi(const char *str);
+int			ft_isdigit(int c);
+
 #endif
