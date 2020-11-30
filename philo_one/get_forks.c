cb561ed5de808ee13e2a5ab5a6bf7656588630e7
Roman <thekukinpower@gmail.com>
2020-12-01 00:23:35 +0300

    add get forks

---
+++ philo_one/get_forks.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_forks.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkristie <mkristie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 00:21:56 by mkristie          #+#    #+#             */
+/*   Updated: 2020/12/01 00:21:57 by mkristie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+static int	choose_a_fork(t_philo *philo)
+{
+	if (philo->num % 2 == 0)
+		return (RIGHT);
+	else
+		return (LEFT);
+}
+
+static void	get_fork(t_philo *philo, int fork)
+{
+	if (fork == RIGHT)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		philo->is_right_taken = 1;
+	}
+	else
+	{
+		pthread_mutex_lock(philo->left_fork);
+		philo->is_left_taken = 1;
+	}
+	print_message(get_time() - start_time, philo->num + 1, TAKEN_A_FORK);
+}
+
+void		get_forks(t_philo *philo)
+{
+	if (choose_a_fork(philo) == RIGHT)
+	{
+		get_fork(philo, RIGHT);
+		get_fork(philo, LEFT);
+	}
+	else
+	{
+		get_fork(philo, LEFT);
+		get_fork(philo, RIGHT);
+	}
+}
